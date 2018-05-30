3a1e87fb5d9a10a63ac4f3ef2ca8c2992497d6d1
Georg Schmidt <gs-develop@gs-sys.de>
2018-05-31 01:42:39 +0200

    limit DSA key to 8192 bit

--- apps/gendsa.c
+++ apps/gendsa.c
@@ -117,6 +117,12 @@ int gendsa_main(int argc, char **argv)
         goto end2;
 
     DSA_get0_pqg(dsa, &p, NULL, NULL);
+
+    if (BN_num_bits(p) > 8192) {
+        BIO_printf(bio_err, "The DSA key size of %d bits is currently not supported. The limit is 8192 bit.\n", BN_num_bits(p));
+        goto end;
+    }
+
     BIO_printf(bio_err, "Generating DSA key, %d bits\n", BN_num_bits(p));
     if (!DSA_generate_key(dsa))
         goto end;
